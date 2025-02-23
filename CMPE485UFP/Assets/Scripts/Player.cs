8cd6b9854aabd700767d4ab43362339b15f4dab7
Güney Yüksel <guney@underheavenstudios.com>
2025-02-23 19:28:25 +0300

    2.11 Making objects respond to inputs
    I've added a new cube that isn't affected from gravity that moves with WASD, you can ascend with E and descend with Q, rotates with C and Z keys too, all from cameras perspective. All movements and rotations are clamped with experimental values.

---
+++ CMPE485UFP/Assets/Scripts/Player.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private Rigidbody rb;
+    [SerializeField] private float moveForce = 10f;
+    [SerializeField] private float verticalForce = 10f;
+    [SerializeField] private float rotationSpeed = 50f;
+    [SerializeField] private Transform cameraTransform;
+    [SerializeField] private float maxSpeed = 5f;
+    [SerializeField] private float maxAngularSpeed = 10f;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        ApplyMovement();
+        ApplyRotation();
+    }
+
+    void ApplyMovement()
+    {
+        float moveX = Input.GetAxis("Horizontal"); // A (-1) / D (1)
+        float moveZ = Input.GetAxis("Vertical");   // W (1) / S (-1)
+        float moveY = 0f;
+
+        if (Input.GetKey(KeyCode.E)) moveY = 1f; // Ascend
+        if (Input.GetKey(KeyCode.Q)) moveY = -1f; // Descend
+
+        Vector3 moveDirection = (cameraTransform.forward * moveZ) + (cameraTransform.right * moveX) + (cameraTransform.up * moveY);
+
+        moveDirection.Normalize();
+
+        rb.AddForce(moveDirection * moveForce, ForceMode.Force);
+
+        rb.velocity = Vector3.ClampMagnitude(rb.velocity, maxSpeed);
+    }
+
+    void ApplyRotation()
+    {
+        if (Input.GetKey(KeyCode.C))
+        {
+            rb.AddTorque(Vector3.up * rotationSpeed, ForceMode.Force);
+            rb.angularVelocity = Vector3.ClampMagnitude(rb.angularVelocity, maxAngularSpeed);
+        }
+        if (Input.GetKey(KeyCode.Z))
+        {
+            rb.AddTorque(Vector3.down * rotationSpeed, ForceMode.Force);
+            rb.angularVelocity = Vector3.ClampMagnitude(rb.angularVelocity, maxAngularSpeed);
+        }
+    }
+}
