6112279cc6fce11a403fdcca92aede86dad52e30
Güney Yüksel <guney@underheavenstudios.com>
2025-03-16 18:30:59 +0300

    Guard for the Invisible Part
    Created the guard for the Ice segment of the part. This guard detects you if you're turn your front to him. When it detects you, it teleports you to the beginning, while also damaging you and removing every breadcrumb so far..

---
+++ Assignment2/Assets/_Game/Scripts/Guard Invisible.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GuardInvisible : MonoBehaviour
+{
+    private bool bActive = false;
+    private Transform playerTransform;
+    private Vector3 guardForward;
+    [SerializeField] private Teleport tp;
+
+    void Start()
+    {
+        playerTransform = GameObject.FindGameObjectWithTag("Player")?.transform;
+
+        guardForward = new Vector3(transform.forward.x, 0f, transform.forward.z).normalized;
+    }
+
+    void Update()
+    {
+        if (bActive && playerTransform != null)
+        {
+            Vector3 playerForward = new Vector3(playerTransform.forward.x, 0f, playerTransform.forward.z).normalized;
+
+            float angle = Vector3.Angle(playerForward, guardForward);
+
+            if (angle < 135f)
+            {
+                tp.TeleportPlayer(true);
+                bActive = false;
+            }
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            bActive = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            bActive = false;
+        }
+    }
+}
